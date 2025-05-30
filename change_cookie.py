b349f175580f0799a554512774b1a0c89731502b
akio0406 <rjkyooooot@gmail.com>
2025-05-30 18:17:31 +0800

    Add or update change_cookie.py

---
+++ change_cookie.py
@@ -0,0 +1,12 @@
+def get_cookies():
+    return {
+      "_ga_1M7M9L6VPX": "GS1.1.1746084569.9.0.1746084570.0.0.0",
+        "_ga": "GA1.1.1216255854.1744635870",
+        "_ga_1M7M9L6VPX": "GS1.1.1744463994.8.1.1744464047.0.0.0",
+        "token_session": "c73c001a5df1e2ff6487f23a32d1d8985ebec80406f5a8ba7f18e822df377891ff4d0873198bf9ab6a7757b9cc353a72",
+        "_ga_1M7M9L6VPX": "GS1.1.1745412925.1.0.1745412925.0.0.0",
+        "_ga_1M7M9L6VPX": "GS1.1.1745412764.2.0.1745412764.0.0.0",
+        "datadome": "hulUlALRgR0XLtg14gSwOPpgx_vMUqbiD8lS_Ddw5V8CvMJZ21w1moOw~CvGDCTgx6EYPxjMualAUDkk4IEs2DQHMQpniA5vIbZklXJRj~HeSQSecpxcUNqh_fxH3N0U"
+    }
+
+# Example usage:
